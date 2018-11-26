7bfd275bd7aacf73f09bcc6ed544baff255cd6c0
Diego Morais <arthur.vinx@gmail.com>
2018-11-26 18:51:21 -0300

    Add files via upload

--- scriptSup.R
+++ scriptSup.R
@@ -20,15 +20,16 @@ t <- transcriptogramStep1(object = t, expression = exp[, idx],
 t <- transcriptogramStep2(object = t)
 levels <- groups[idx] %in% "control"
 t <- differentiallyExpressed(object = t, levels = levels, pValue = 0.02,
-                             title = "aza10 x control (Microarray)",
+                             title = "aza10 x aza0 (Microarray)",
                              species = "Homo sapiens",
                              boundaryConditions = FALSE)
+View(DE(t))
 rdp <- clusterVisualization(t, onlyGenesInDE = TRUE)
 t <- clusterEnrichment(t, species = "Homo sapiens", nCores = TRUE,
                            algorithm = "parentchild", pValue = 0.05,
                            onlyGenesInDE = TRUE)
 terms <- Terms(t)
 top5terms <- lapply(1:4, function(i){
-  head(terms[terms$ClusterNumber == i, c(1, 2, 6)], 5)
+  head(terms[terms$ClusterNumber == i, c(1, 2, 7)], 5)
 })
 top5terms
